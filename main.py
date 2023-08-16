4b9eaf53d4a87431393c953e63e9f861009e1d0c
juanB <juancruzberrios73@gmail.com>
2023-08-16 16:34:24 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,75 @@
+#ejercisio 1. Crea una variable llamada "numero1" y asígnale un número entero de tu elección
+
+numero1 = 20
+
+#ejercisio 2. No borres la variable número uno y crea una variable llamada "numero2" asignándole un número decimal de tu elección.
+
+numero2 = 15.5
+
+#ejercisio 3. Crear una variable llamada "suma" y almacena la suma de "numero1" y "numero2".
+
+numero3 = numero1 + numero2
+
+#ejercisio 4. Ahora crear tres variables más. Una para resta, otra para multiplicación y otra para división. Imprime estas variables.
+
+numero4 = numero1 - numero2
+
+numero5= numero1 * numero2
+
+numero6 = numero1/numero2
+
+print("[ numero4 = ",numero4,"] [ numero5  = ",numero5," ] [numero6 = ",numero6," ]")
+
+#ejercisio 5. Crea una variable llamada "nombre" y asígnale tu nombre como valor.
+
+nombre = "Berrios Juan Cruz"
+
+#ejercisio 6. Crea una variable llamada "precio" y asígnale un valor decimal que represente el precio de un artículo ficticio.
+
+precio = 999.9
+
+#ejercisio 7. crea una variable llamada "descuento" y asígnale un valor decimal que represente el descuento aplicado al artículo
+
+descuento = 0.50 #descuento del 50%
+
+#ejercisio 8. Ahora, intenta calcular el precio final aplicando el descuento al precio original y almacena el resultado en una variable llamada "precio_final"
+
+precio_final = precio * descuento
+
+#ejercisio 9. Crea una variable llamada "cadena" y asignale un texto, una frase, lo que quieras de tu elección. Qué sea un string.
+
+cadena = "M.O.O.N - Dust"
+
+#ejercisio 10. crea una nueva variable llamada "longitud". En ella, almacena la longitud en caracteres de la cadena utilizando una de las funcion leg
+
+longitud = len(cadena)
+
+#ejercisio 11. 11. Crea otra vez la variable llamada "precio" y dale un valor decimal, el que sea y conviértelo en número entero.
+
+precio = int(500.50)
+
+#ejercisio 12. Crea dos variables. Una se va a llamar "nombre" y la segunda "apellido" concaténalas en una tercera variable llamada "nombre_completo".
+
+nombre = "Juan Cruz"
+
+apellido = "Berrios"
+
+nombre_completo = nombre + " " + apellido
+
+#ejercisio 13. Escribe tu edad en una variable. Increméntala en 5 y luego disminúyela en 10.
+
+edad = 20 + 5 - 10
+
+#ejercisio 14. Crea una variable llamada "altura" que contenga  tu altura en metros y cm. Multiplícala por 4 y luego divídela en 3.
+
+altura = 1.80 * 4 / 3
+
+#ejercisio 15. Crea una variable que contenga tu nombre completamente en mayúsculas. Después transfórmalo todo en minúsculas con algún método o función de Python
+
+nombre = "BERRIOS JUAN CRUZ"
+
+nombre1 = nombre.lower()
+
+#ejercisio 16. con la variable con el nombre en mayúsculas, transforme todo en minúsculas excepto la primera letra.
+
+nombre2 = nombre.title()
