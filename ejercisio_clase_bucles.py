4af8117c43675b8ae8e748fa7ee7ad29a3a0b6c7
Juan Cruz B <106563641+juanCruzB73@users.noreply.github.com>
2023-08-29 11:56:34 -0300

    Update ejercisio_clase_bucles.py

--- ejercisio_clase_bucles.py
+++ ejercisio_clase_bucles.py
@@ -15,3 +15,30 @@ for i in range(1):
 
 
   print(palabra_encriptada)
+  #ejercisio 2
+  ''' Crear un programa que solicite el ingreso de números enteros positivos, hasta que el usuario ingrese el
+0. Por cada número, informar cuántos dígitos pares y cuántos impares tiene.
+Al finalizar, informar la cantidad de dígitos pares y de dígitos impares leídos en total.'''
+
+n = 1
+dig_pares = 0
+dig_inpares = 0
+while n != 0:
+  n = int(input("INGRESE UN NUMERO par postivo "))
+  if (n % 2 != 0):
+    print("numero no valido")
+  else:
+    auxiliar = n
+    dig_pares_p = 0
+    dig_inpares_p = 0
+    while auxiliar > 0:
+      digito = auxiliar % 10
+      if (digito % 2 == 0):
+        dig_pares = dig_pares + 1
+        dig_pares_p = dig_pares_p + 1
+      else:
+        dig_inpares_p = dig_inpares_p + 1
+        dig_inpares = dig_inpares + 1
+      auxiliar = auxiliar / 10
+    print("hay ", dig_pares_p, " pares y ", dig_inpares_p, " inpares")
+print("hay ", dig_pares, " pares y ", dig_inpares, " inpares")
