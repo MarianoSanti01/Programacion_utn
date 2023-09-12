dcf5ce400fef1d2f30967dfac84003fefe720964
juanB <juancruzberrios73@gmail.com>
2023-09-12 15:53:29 -0300

    modificaciones a practica para parcial

--- repaso_parcial.py
+++ repaso_parcial.py
@@ -1,33 +1,43 @@
 #BLock de notas
+#modificación 1 de cuando se mostro en clase, ahora la opcion 2 no borra todo lo escrito, pide cuantas palabras se borraran y elemina ese numero
+#modificación 2 se elimino la opcion de mostrar lo escrito para mostrarlo simpre despues de realizar alguna operacion.
 content = ""
 while True:
-    print("\n menu de operaciones: \n 1.añadir texto \n 2.borrar documento \n 3.buscar cuantas veces se imprimio una palabra \n 4.leer lo ingresado \n 5.salir del programa")
+    print("\n menu de operaciones: \n 1.añadir texto \n 2.borrar texto \n 3.buscar cuantas veces se imprimio una palabra \n 4.salir del programa \n ")
     operation=input()
     if(operation == "1"):
         print("texto a ingresar: \n")
         new_text=input()
-        content += ' / '+new_text
+        content = content+new_text+' '
+        content_arr = content.split(" ")
     elif (operation == "2"):
         if (content == ""):
             print("no hay nada que revertir")
-        else:			
-            content = ""
+        else:
+            deleate=int(input("cuantas palabras borrara?"))
+            content=""
+            index=len(content_arr)-deleate
+            if(index<0):
+                print("no se pueden borrar tantas palabras")
+            else:
+                while len(content_arr)>=index:
+                    content_arr.pop()
+                for i in range(len(content_arr)):
+                    content += content_arr[i]+" "
     elif (operation == "3"):
         print("¿que palabra desea buscar?")
         word=input().lower()
         counter =0
-        value=0
-        ar_content=content.split(" ")
-        for world in ar_content:
-            if(ar_content[value].lower()==word):
+        i=0
+        for i in range(len(content_arr)):
+            if(content_arr[i].lower()==word):
                 counter += 1
-                value +=1
+                i+=1
             else:
-                value +=1
+                i+=1
         print("la palabra ",word, "se muestra un total de: ", counter," veces")
     elif(operation == "4"):
-        print(content)
-    elif(operation == "5"):
         break
     else:
         print("operacion invalida")
+    print(content)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,22 +1,6 @@
-n = 1
-dig_pares = 0
-dig_inpares = 0
-while n != 0:
-  n = int(input("INGRESE UN NUMERO POSITIVO - PARA FINALIZAR INGRESE 0"))
-  if (n < 0):
-    print("numero no valido")
-  else:
-    auxiliar = n
-    dig_pares_p = 0
-    dig_inpares_p = 0
-    while auxiliar > 0:
-      digito = auxiliar % 10
-      if (digito % 2 == 0):
-        dig_pares = dig_pares + 1
-        dig_pares_p = dig_pares_p + 1
-      else:
-        dig_inpares_p = dig_inpares_p + 1
-        dig_inpares = dig_inpares + 1
-      auxiliar = math.floor(auxiliar/10)
-    print("hay ", dig_pares_p, " pares y ", dig_inpares_p, " inpares")
-print("hay ", dig_pares, " pares y ", dig_inpares, " inpares")
+list = [1,2,5,3,5]
+list2=["1","2","5"]
+st=""
+for i in range(len(list2)):
+    st += list2[i]+" "
+print(st)
